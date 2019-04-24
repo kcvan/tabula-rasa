730a0047d689cef8062375c10a2fca736308a0a3
Kevin Van <kcongvan@gmail.com>
2019-04-24 00:00:23 -0700

    feat(post): add post request example

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as React from 'react';
 
 import { responseResolver } from '../../utils/responseResolver/responseResolver';
@@ -8,26 +9,46 @@ import './home.scss';
 // using class component
 
 // interface State {
-//   status: string
+//   reconsidering: boolean;
+//   reconsiderStatus: string;
+//   status: string,
 // }
 
 // class Home extends React.Component {
 //   readonly state: State = {
+//     reconsidering: false,
+//     reconsiderStatus: 'Will I reconsider?',
 //     status: ''
 //   };
 
 //   componentDidMount() {
-//     responseResolver('/api/thanos')
+//     responseResolver('/api/thanos/decimate')
 //       .then((res: string[]) => this.setState({ status: res }))
 //       .catch((err: Error) => console.log(err));
 //   }
 
 //   render() {
+//     const { reconsidering, reconsiderStatus, status } = this.state;
 //     return (
 //       <div className="home">
-//         <h1>{this.state.status}</h1>
+//         <h1>{status}</h1>
+//         <h2>Thanos: {reconsiderStatus}</h2>
+//         <button onClick={this.reconsider} disabled={reconsidering}>
+//           {reconsidering ? 'Reconsidering...' : 'Pls reconsider'}
+//         </button>
 //       </div>
 //     );
+//   }
+
+//   reconsider = async () => {
+//     this.setState({ reconsidering: true });
+//     try {
+//       const response = await axios.post('api/thanos/reconsider', { reconsider: Math.floor(Math.random() * 100) < 50 ? true : false });
+//       this.setState({ reconsidering: false });
+//       this.setState({ reconsiderStatus: response.data});
+//     } catch (e) {
+//       console.log(`Axios request failed: ${e}`);
+//     }
 //   }
 // }
 
@@ -36,9 +57,11 @@ import './home.scss';
 
 const Home: React.FC = () => {
   const [status, setStatus] = React.useState<string>('');
+  const [reconsiderStatus, setReconsiderStatus] = React.useState<string>('Will I reconsider?');
+  const [reconsidering, setReconsidering] = React.useState<boolean>(false);
 
   React.useEffect(() => {
-    responseResolver('/api/thanos')
+    responseResolver('/api/thanos/decimate')
       .then((res: string) => setStatus(res))
       .catch((err: Error) => console.log(err));
   }, []);
@@ -46,8 +69,21 @@ const Home: React.FC = () => {
   return (
     <div className="home">
       <h1>{status}</h1>
+      <h2>Thanos: {reconsiderStatus}</h2>
+      <button onClick={reconsider}>{reconsidering ? 'Reconsidering...' : 'Pls reconsider'}</button>
     </div>
   );
+
+  async function reconsider() {
+    setReconsidering(true);
+    try {
+      const response = await axios.post('api/thanos/reconsider', { reconsider: Math.floor(Math.random() * 100) < 50 ? true : false });
+      setReconsidering(false);
+      setReconsiderStatus(response.data);
+    } catch (e) {
+      console.log(`Axios request failed: ${e}`);
+    }
+  }
 }
 
 export default Home;
